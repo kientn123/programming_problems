ed5ea35812dfae38f9c54fdae19e49134f1ae635
Kien Nguyen <kiennt.sic@gmail.com>
2014-07-17 23:44:44 -0500

    5.1.crack

---
+++ 5.1.py
@@ -0,0 +1,17 @@
+'''
+You are given 32-bit numbers, N and M, and two bit positions, i and j. Write a method to set all bits between i and j in N equal to M (e.g., M becomes a substring of N located at i and starting at j).
+Example:
+input: N = 10000000000, M = 10101, i = 2, j = 6
+output: N = 10001010100
+'''
+def update(n, m, i, j):
+  i_pos = n >> i
+  i_pos <<= i
+  j_pos = n >> (j + 1)
+  j_pos <<= (j + 1)
+  between = i_pos - j_pos
+  return (n & between) & (m << j)
+
+n = 10000000000
+m = 10101
+print update(n, m, 2, 6)
