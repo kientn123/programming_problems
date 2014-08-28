4243ec69ed6926c62862e1e4aa499edaafa1c803
Kien Nguyen <kiennt.sic@gmail.com>
2014-08-27 19:13:09 -0500

    8.3.epi redo

--- epi/Ruby/8.3.epi.rb
+++ epi/Ruby/8.3.epi.rb
@@ -4,21 +4,28 @@ determine if a singly linked list is cyclic
 
 class LinkedList
   def self.is_cyclic(l)
-    if l.next.nil?
-      return "nil"
+    if l.next.nil? or l.next.next.nil?
+      return nil
     else
-      start = l.next
-      runner = start
+      runner1 = l
+      runner2 = l
       while true
-        runner = runner.next
-        if runner.nil?
+        if runner2.next.nil? or runner2.next.next.nil?
           return "nil"
-        elsif runner == start
-          return start
         else
-          next
+          runner1 = runner1.next
+          runner2 = runner2.next.next
+          if runner1 == runner2
+            break
+          end
         end
       end
+      runner1 = l
+      while runner1 != runner2
+        runner1 = runner1.next
+        runner2 = runner2.next
+      end
+      return runner1
     end
   end
 end
@@ -37,5 +44,5 @@ l = Node.new(nil, Node.new(1, Node.new(2, Node.new(3, nil))))
 tail = Node.new(8, nil)
 start = Node.new(5, Node.new(6, Node.new(7, tail)))
 tail.next = start
-f = Node.new(nil, start)
-puts LinkedList.is_cyclic(f)
+f = Node.new(nil, Node.new(1, Node.new(2, Node.new(3, Node.new(4, start)))))
+puts LinkedList.is_cyclic(l)
