776f53bd47145b712eda6cfac1fca4944d9fd57d
Kien Nguyen <kiennt.sic@gmail.com>
2014-08-31 10:36:02 -0500

    find k in sorted array; duplicates allowed

---
+++ epi/Ruby/12.1.epi.rb
@@ -0,0 +1,24 @@
+=begin
+Return the first occurrance of k in a sorted array (duplicates allowed)
+=end
+
+def find_k(arr, k)
+	left = 0
+	right = arr.length - 1
+	result = -1
+	until left > right
+		mid = (left + right) / 2
+		if arr[mid] == k
+			result = mid
+			right = mid - 1
+		elsif arr[mid] > k
+			right = mid - 1
+		else
+			left = mid + 1
+		end
+	end
+	result
+end
+
+a = [1, 1, 2, 3, 3, 4, 5, 6, 6, 6, 7]
+puts find_k(a, 3)
