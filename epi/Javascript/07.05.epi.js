4e33b7a4a2a98519e292e6d6ff22c76980adfead
Kien Nguyen <kiennt.sic@gmail.com>
2014-10-13 18:32:13 -0500

    mnemonics for a phone number

---
+++ epi/Javascript/07.05.epi.js
@@ -0,0 +1,34 @@
+/*
+Compute all mnemonics for a phone number
+*/
+function mnemonics(str) {
+  hash = {
+    '2': ['A', 'B', 'C'],
+    '3': ['D', 'E', 'F'],
+    '4': ['G', 'H', 'I'],
+    '5': ['J', 'K', 'L'],
+    '6': ['M', 'N', 'O'],
+    '7': ['P', 'Q', 'R', 'S'],
+    '8': ['T', 'U', 'V'],
+    '9': ['W', 'X', 'Y', 'Z']
+  };
+
+  res = [];
+
+  function compute(aStr, start, store) {
+    if (start >= aStr.length) {
+      res.push(store)
+    } else {
+      list = hash[aStr[start]];
+      for (var i=0; i<list.length; i++) {
+        console.log("char is: " + list[i]);
+        compute(aStr, start+1, store.concat([list[i]]));
+      }
+    }
+  };
+
+  compute(str, 0, []);
+  return res;
+}
+
+console.log(mnemonics('23').length);
