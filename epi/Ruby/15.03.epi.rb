f5c754749b0e9d2c882c1b18cf504c9a866377a5
Kien Nguyen <kiennt.sic@gmail.com>
2014-10-26 21:13:55 -0500

    find the first key larger than k in a BST

--- epi/Ruby/15.03.epi.rb
+++ epi/Ruby/15.03.epi.rb
@@ -1,22 +1,27 @@
 =begin
-Test if a binary tree satisfies the BST property
-BST property: the key stored at a node is greater than or equal to the keys stored at the
-nodes of its left subtree and less than or equal to the keys stored in the nodes of its
-right subtree.
+Find the first key larger than k in a BST
+Write a function that takes a BST T and a key k, and returns the first entry larger than k
+that would appear in an inorder traversal. If k is absent or no key larger than k is
+present, return nil.
 =end
-def is_binary_tree(root)
-  return is_binary_tree_helper(root, -Float::INFINITY, Float::INFINITY)
-end
-
-def is_binary_tree_helper(root, lower, upper)
-  if root.nil?
-    return true
-  elsif root.data < lower || root.data > upper
-    return false
-  else
-    return is_binary_tree_helper(root.left, lower, root.data) &&
-      is_binary_tree_helper(root.right, root.data, upper)
+def first_larger_than_k(root, k)
+  return nil if root.nil?
+  target = root
+  k_present = false
+  res = nil
+  while target
+    if target.data == k
+      k_present = true
+      target = target.right
+    elsif target.data < k
+      target = target.right
+    else # target > k
+      res = target
+      target = target.left
+    end
   end
+
+  return k_present ? res : nil
 end
 
 class Node
@@ -38,4 +43,5 @@ a.left = b; a.right = i; b.left = c; b.right = f; c.left = d; c.right = e
 f.right = g; g.left = h; i.left = j; j.right = k; k.left = l; k.right = n
 l.right = m; i.right = o; o.right = q
 
-puts is_binary_tree(a)
+puts first_larger_than_k(a, 23).name
+puts first_larger_than_k(a, 32)
