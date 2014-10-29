1db89d46a8843fbf49a2737c526f72d18e2ade97
Kien Nguyen <kiennt.sic@gmail.com>
2014-10-28 22:41:20 -0500

    test if binary tree is min-first BST

---
+++ epi/Python/15.20.epi.py
@@ -0,0 +1,27 @@
+# Test if a binary tree is a min-first BST
+# A min-first bst is one in which the minimum key is stored at the root; each key in the left subtree
+# is less than every key in the right substree. The subtrees themselves are min-first BSTs
+# Write a function that takes a min-first BST T and a key k, and returns true iff T contains k
+def contains(root, k):
+    if root == None:
+        return False
+    if root.data == k:
+        return True
+    if root.right and root.right.data <= k:
+        return contains(root.right, k)
+    else:
+        return contains(root.left, k)
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+if __name__ == "__main__":
+    a = Node(2); b = Node(3); c = Node(13); d = Node(5); e = Node(17); f = Node(19)
+    g = Node(7); h = Node(11); i = Node(23)
+    a.left = b; a.right = c; b.right = d; c.left = e; c.right = f
+    d.left = g; d.right = h; f.left = i
+    print contains(a, 5)
+    print contains(a, 20)
