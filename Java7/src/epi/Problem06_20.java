a44d167bf0bfde5e44fda44b4a87beb3d72dfc49
Kien Nguyen <kiennt.sic@gmail.com>
2014-11-03 00:08:55 -0600

    add back java

---
+++ Java7/src/epi/Problem06_20.java
@@ -0,0 +1,18 @@
+package epi;
+import java.util.*;
+
+/*
+ * Generate nonuniform random numbers
+ * You're given a set T of n distinct real numbers (t0,t1,..,tn-1) and probabilities p0,p1,..,pn-1,
+ * which sum up to 1. Assume that t0<t1<..<tn-1. Given a random number generator that produces
+ * values in [0,1] uniformaly, how would you generate a number in T according to the specified
+ * probabilities?
+ */
+public class Problem06_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
